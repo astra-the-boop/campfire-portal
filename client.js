51799f553d77484da55349db4c38b32cded4ae5b
astra-the-boop <astra.the.boop@icloud.com>
2025-12-25 04:14:16 +0700

    I HATE CORS I HATE CORS ASFKGKJGASFNKGBERNKABERNKERALNKNAEGLF

--- client.js
+++ client.js
@@ -1,5 +1,15 @@
+
 async function getToken(){
-    const response = await fetch("/get-token");
+    const response = await fetch("https://localhost:3679/token",{
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify({
+            room: "campfire",
+            identity: "astra-123"
+        })
+    });
+    console.log(await response.text());
+
     const data = await response.json();
     return data.token;
 }
@@ -13,7 +23,25 @@ async function joinRoom(){
         console.log(`connected to ${room.name}`);
 
         room.participants.forEach(participant => {
-            console.log()
+            console.log(`Participants joined: ${participant.identity}`);
+            participant.tracks.forEach(trackPub => {
+                if(trackPub.isSubscribed){
+                    document.body.append(trackPub.track.attach());
+                }
+            });
+
+            participant.on("trackSubscribed", track => {
+                document.body.appendChild(track.attach());
+            })
         })
+
+        room.on("ParticipantConnected", participant => {
+            console.log(`new participant ${participant.identity}`);
+        })
+
+    }catch(err){
+        console.log(`failed: ${err}`)
     }
 }
+
+joinRoom();
